2ac1b0a6ad820dff9bb2ac2e809239582708a9f0
Eric Stafford <eric.stafford@paragon-inc.com>
2017-04-07 11:25:47 -0400

    RTE Bug Fix

--- src/Foundation/ContentEditor/code/Pipelines/RichTextContent/RichText.cs
+++ src/Foundation/ContentEditor/code/Pipelines/RichTextContent/RichText.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using HtmlAgilityPack;
 
 namespace EditorEnhancementToolkit.Foundation.ContentEditor.Pipelines.RichTextContent
 {
-    //TODO: Discuss whether to add option to completely remove all dimensions both inline and regular attribute
     public static class RichText
     {
         public static string Content(string content)
@@ -26,7 +23,7 @@ namespace EditorEnhancementToolkit.Foundation.ContentEditor.Pipelines.RichTextCo
                     continue;
 
                 var alteredStyles = new StringBuilder();
-                var inlineStyleList = imgAttr.Value.Split(';').Where(x => x.Contains("width") || x.Contains("height"));
+                var inlineStyleList = imgAttr.Value.Split(';');
 
                 if(!inlineStyleList.Any())
                     continue;
@@ -41,14 +38,12 @@ namespace EditorEnhancementToolkit.Foundation.ContentEditor.Pipelines.RichTextCo
                     var property = attrList[0].Trim();
                     var val = attrList[1].Trim();
 
-                    //if (!RemoveImageDimensions() && (property.Equals("width", StringComparison.InvariantCultureIgnoreCase) || property.Equals("height", StringComparison.InvariantCultureIgnoreCase)))
                     if (property.Equals("width", StringComparison.InvariantCultureIgnoreCase) || property.Equals("height", StringComparison.InvariantCultureIgnoreCase))
                         img.Attributes.Add(property, val);
                     else
                         alteredStyles.Append($"{property}:{val}; ");
                 }
 
-                // If the inline style attr contains other styles than the dimensions place those back in
                 if (alteredStyles.Length > 0)
                     img.Attributes["style"].Value = alteredStyles.ToString();
                 else
@@ -57,27 +52,5 @@ namespace EditorEnhancementToolkit.Foundation.ContentEditor.Pipelines.RichTextCo
 
             return doc.DocumentNode.OuterHtml;
         }
-
-        //private static HtmlNode ImageDimensions(HtmlNode imgNode)
-        //{
-        //    if(!RemoveImageDimensions())
-        //        return imgNode;
-            
-        //    var width = imgNode.Attributes["width"]?.Value;
-        //    var height = imgNode.Attributes["height"]?.Value;
-
-        //    if(!string.IsNullOrEmpty(width))
-        //        imgNode.Attributes["width"].Remove();
-
-        //    if(!string.IsNullOrEmpty(height))
-        //        imgNode.Attributes["height"].Remove();
-
-        //    return imgNode;
-        //}
-
-        //private static bool RemoveImageDimensions()
-        //{
-        //    return Settings.GetBoolSetting("EnhanceTheEditorExperience.RichTextEditor.RemoveDimensions", false);
-        //}
     }
 }
